a397a2beb4d2a672845a3823b7f93030a6211869
Olivia James <LOlivia.James@gmail.com>
2015-11-16 03:58:48 -0500

    working towards upgraded version, part I

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <vector>
 #include <map>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -13,6 +14,7 @@ map <string, int> ::iterator valid_entry;
 void create_translation();
 void display_menu();
 void execute_instructions(int opcode);
+char additional_instructions ();
 
 
 int ACC, PC=0, opcode, operand, result;
@@ -20,14 +22,16 @@ vector<int> RAM;
 
 int main()
 {
-    string filename, entry;
+    string filename, entry, user_added_instruct;
+    char more_instruct_response;
+    int new_operand;
 
     filename = filename_prompt();
     ifstream myfile(filename.c_str());
 
     create_translation();
 
-    if(myfile.is_open()){
+    if (myfile.is_open()){
         while (myfile >> entry && !myfile.eof()){
             valid_entry = translation.find(entry);
             if (valid_entry == translation.end()){
@@ -40,19 +44,52 @@ int main()
         /*for (int i=0; i<RAM.size(); i++){
         cout << RAM[i] <<endl;
         }*/
-        for(;;){     // forever
+        cout << RAM.size() <<endl;
+        for(int i=0; i<RAM.size(); i++){     // forever
             // FETCH CYCLE
             opcode = RAM[PC];
             execute_instructions(opcode);
         }
-    }
-    else{
-        cout << "Error in opening your file, try again." << endl;
-        filename_prompt();
+
+        more_instruct_response = additional_instructions();
+        if (more_instruct_response =='Y' || more_instruct_response =='y'){
+                display_menu();
+                cin >> user_added_instruct >> new_operand;
+                valid_entry = translation.find(user_added_instruct);
+                {if (valid_entry == translation.end()){
+                    //istringstream(entry) >> result;
+                    RAM.push_back(new_operand);
+                    //RAM.push_back(100);
+                    }
+                else
+                    RAM.push_back(valid_entry->second);
+                    //RAM.push_back(1001);
+                }
+        cout << RAM.size() <<endl;
+        for (int i=0; i<RAM.size(); i++){
+        cout << RAM[i] <<endl;
+        }
+        /*for(int i=0; i<RAM.size(); i++){     // forever
+            // FETCH CYCLE
+            opcode = RAM[PC];
+            execute_instructions(opcode);
+        }*/
+                }
+        else{
+            cout << "Exiting program." << endl;
+            exit (EXIT_SUCCESS);
+        }
+        }
+
+
+    else {
+        //while (myfile.fail()){
+            cout << "Error in opening your file, try again." << endl;
+            //filename_prompt();
+        //}
     }
 
     myfile.close();
-
     return 0;
 }
 
@@ -134,12 +171,21 @@ void execute_instructions(int opcode){
 }
 
 void display_menu(){
-    cout << "Enter CLR to clear last calculated value" << endl;
-    cout << "Enter LOAD to load an operand to the accumulator." << endl;
-    cout << "Enter ADD to add operand to last calculated value." << endl;
-    cout << "Enter SUB to subtract operand from last calculated value." << endl;
-    cout << "Enter MUL to multiply last calculated value by an operand." << endl;
-    cout << "Enter DIV to divide last calculated value by an operand." << endl;
-    cout << "Enter OUT to display last calculated value." << endl;
+    cout << "Enter CLR to clear last calculated value, OR" << endl;
+    cout << "Enter LOAD to load an operand to the accumulator, OR" << endl;
+    cout << "Enter ADD to add operand to last calculated value, OR" << endl;
+    cout << "Enter SUB to subtract operand from last calculated value, OR" << endl;
+    cout << "Enter MUL to multiply last calculated value by an operand, OR" << endl;
+    cout << "Enter DIV to divide last calculated value by an operand, OR" << endl;
+    cout << "Enter OUT to display last calculated value, OR" << endl;
     cout << "Enter HALT to halt program." << endl;
+    cout << endl;
+    cout << "Then enter the operand: " <<endl;
+}
+
+char additional_instructions (){
+    char response;
+    cout << "Do you have additional instructions? If so, enter 'Y'. " << endl;
+    cin >> response;
+    return response;
 }
