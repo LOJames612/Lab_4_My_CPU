5ce3120c3b90aacf1d130463de09b47d29f66f12
Olivia James <LOlivia.James@gmail.com>
2015-11-17 15:24:07 -0500

    threw most of main into a function

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ using namespace std;
 string filename_prompt();
 map <string, int> translation;
 map <string, int> ::iterator valid_entry;
+void upload_program();
 void create_translation();
 void display_menu();
 void execute_instructions(int opcode);
@@ -19,20 +20,114 @@ char additional_instructions ();
 
 int ACC, PC=0, opcode, operand, result;
 vector<int> RAM;
+string filename, entry, user_added_opcode;
+char more_instruct_response;
+int new_operand;
 
 int main()
 {
-    string filename, entry, user_added_opcode;
-
-    char more_instruct_response;
-    int new_operand;
-
-    filename = filename_prompt();
 
     create_translation();
 
+
+    filename = filename_prompt();
+
     while (true)
     {
+        upload_program();
+    /*ifstream myfile(filename.c_str());
+    if (myfile.is_open()){
+        cout << "File is open";
+        do {
+            (myfile >> entry && !myfile.eof());//{
+            valid_entry = translation.find(entry);
+            if (valid_entry == translation.end()){
+                istringstream(entry) >> result;
+                RAM.push_back(result);
+                }
+            else
+                RAM.push_back(valid_entry->second);
+        } while (entry != "HALT");
+        cout << RAM.size() <<endl;
+        //cout <<endl;
+
+        //for (int i=0; i<RAM.size(); i++){
+        //cout << RAM[i] <<endl;
+        //}
+
+        for(int i=0; i<RAM.size(); i++){
+            // Fetch cycle
+            opcode = RAM[PC];
+            execute_instructions(opcode);
+        }
+
+
+        more_instruct_response = additional_instructions();
+
+        while (more_instruct_response =='Y' || more_instruct_response =='y'){
+                if (RAM[(RAM.size())-1]==9)
+                    RAM.erase (RAM.begin()+(RAM.size())-1);
+                    display_menu();
+                    //do {
+                    cin >> user_added_opcode;   //MAKE USER INPUT UPPER CASE!
+                    valid_entry = translation.find(user_added_opcode);
+                    if (valid_entry->first == "CLR" || valid_entry->first == "OUT" || valid_entry->first == "HALT"){
+                        cout << "OK then..." <<endl;
+                        RAM.push_back(valid_entry->second);
+                    }
+                    else{
+                        cout << "Step 2: Enter the operand: " <<endl;
+                        cin >> new_operand;
+                        RAM.push_back(new_operand);
+                    }
+                    //for (int i=0; i<RAM.size(); i++){
+                    //cout << RAM[i] <<endl;
+                    //}
+                    for(int i=0; i<RAM.size(); i++){
+                    // FETCH CYCLE
+                    opcode = RAM[PC];
+                    execute_instructions(opcode);
+                    }
+
+                    more_instruct_response = additional_instructions();
+        }
+        if (more_instruct_response !='Y' || more_instruct_response !='y'){
+            cout << "Exiting current program..." << endl;
+            cout << "Returning to main menu..." <<endl;     //GO BACK TO MAIN MENU!
+            exit (EXIT_SUCCESS);
+            }
+        }
+    else{
+        cout << "Error in opening your file, try again." << endl;
+        cin >> filename;
+        }*/
+    }
+    return 0;
+}
+
+
+string filename_prompt(){
+    string textfile;
+
+    cout << "Enter the filename: " << endl;
+    cin >> textfile;
+    return textfile;
+}
+
+void create_translation(){
+    translation ["CLR"] = 0;
+    translation ["LOAD"] = 1;
+    translation ["ADD"] = 2;
+    translation ["SUB"] = 3;
+    translation ["MUL"] = 4;
+    translation ["DIV"] = 5;
+    translation ["OUT"] = 6;
+    translation ["JMP"] = 7;
+    translation ["BNZ"] = 8;
+    translation ["HALT"] = 9;
+}
+
+void upload_program(){
     ifstream myfile(filename.c_str());
     if (myfile.is_open()){
         cout << "File is open";
@@ -61,151 +156,44 @@ int main()
 
 
         more_instruct_response = additional_instructions();
-        //if (more_instruct_response =='Y' || more_instruct_response =='y'){
+
         while (more_instruct_response =='Y' || more_instruct_response =='y'){
                 if (RAM[(RAM.size())-1]==9)
                     RAM.erase (RAM.begin()+(RAM.size())-1);
-                display_menu();
-                //do {
-                cin >> user_added_opcode;   //MAKE USER INPUT UPPER CASE!
-                valid_entry = translation.find(user_added_opcode);
-                if (valid_entry->first == "CLR" || valid_entry->first == "OUT" || valid_entry->first == "HALT"){
-                    cout << "OK then..." <<endl;
-                    RAM.push_back(valid_entry->second);
+                    display_menu();
+                    //do {
+                    cin >> user_added_opcode;   //MAKE USER INPUT UPPER CASE!
+                    valid_entry = translation.find(user_added_opcode);
+                    if (valid_entry->first == "CLR" || valid_entry->first == "OUT" || valid_entry->first == "HALT"){
+                        cout << "OK then..." <<endl;
+                        RAM.push_back(valid_entry->second);
+                    }
+                    else{
+                        cout << "Step 2: Enter the operand: " <<endl;
+                        cin >> new_operand;
+                        RAM.push_back(new_operand);
+                    }
+                    //for (int i=0; i<RAM.size(); i++){
+                    //cout << RAM[i] <<endl;
+                    //}
+                    for(int i=0; i<RAM.size(); i++){
+                    // FETCH CYCLE
+                    opcode = RAM[PC];
+                    execute_instructions(opcode);
                     }
 
-                else{
-                    //cout << "Nevermind  then..." << endl;
-                    cout << "Enter the operand: " <<endl;
-                    cin >> new_operand;
-                    RAM.push_back(new_operand);
-                    }
-                //}while (user_added_opcode != "HALT");
-                //}
-
-        //for (int i=0; i<RAM.size(); i++){
-        //cout << RAM[i] <<endl;
-        //}
-        for(int i=0; i<RAM.size(); i++){
-            // FETCH CYCLE
-            opcode = RAM[PC];
-            execute_instructions(opcode);
+                    more_instruct_response = additional_instructions();
         }
-        more_instruct_response = additional_instructions();
-        }
-        //else{
         if (more_instruct_response !='Y' || more_instruct_response !='y'){
             cout << "Exiting current program..." << endl;
             cout << "Returning to main menu..." <<endl;     //GO BACK TO MAIN MENU!
             exit (EXIT_SUCCESS);
+            }
         }
-        }
-
-        //break;
-        //}
-
-
-
-    else
+    else{
         cout << "Error in opening your file, try again." << endl;
         cin >> filename;
-    }
-
-    return 0;
-    /*
-
-
-    if (myfile.is_open()){
-        //while (myfile >> entry && !myfile.eof()){
-        do {
-            (myfile >> entry && !myfile.eof());//{
-            valid_entry = translation.find(entry);
-            if (valid_entry == translation.end()){
-                istringstream(entry) >> result;
-                RAM.push_back(result);
-                }
-            else
-                RAM.push_back(valid_entry->second);
-        } while (entry != "HALT");
-        cout << RAM.size() <<endl;
-        //cout <<endl;
-
-        //for (int i=0; i<RAM.size(); i++){
-        //cout << RAM[i] <<endl;
-        //}
-
-        for(int i=0; i<RAM.size(); i++){
-            // Fetch cycle
-            opcode = RAM[PC];
-            execute_instructions(opcode);
         }
-
-        more_instruct_response = additional_instructions();
-        if (more_instruct_response =='Y' || more_instruct_response =='y'){
-                display_menu();
-                do {
-                cin >> user_added_opcode;
-                valid_entry = translation.find(user_added_opcode);
-                {if (valid_entry->first == "CLR" || valid_entry->first == "OUT"} /|| valid_entry->first == "HALT"){
-                    cout << "OK then..." <<endl;
-                    RAM.push_back(valid_entry->second);
-                    }
-                else{
-                    //cout << "Nevermind  then..." << endl;
-                    cout << "Enter the operand: " <<endl;
-                    cin >> new_operand;
-                    RAM.push_back(new_operand);
-                    }
-                }
-                }while (user_added_opcode != "HALT");
-
-        //for (int i=0; i<RAM.size(); i++){
-        //cout << RAM[i] <<endl;
-        //}
-        for(int i=0; i<RAM.size(); i++){
-            // FETCH CYCLE
-            opcode = RAM[PC];
-            execute_instructions(opcode);
-        }
-        }
-        else{
-            cout << "Exiting current program..." << endl;
-            cout << "Returning to main menu..." <<endl;     //GO BACK TO MAIN MENU!
-            exit (EXIT_SUCCESS);
-        }
-        }
-    }
-
-    myfile.close();
-    return 0;
-    */
-}
-
-
-
-
-
-
-
-string filename_prompt(){
-    string textfile;
-
-    cout << "Enter the filename: " << endl;
-    cin >> textfile;
-    return textfile;
-}
-
-void create_translation(){
-    translation ["CLR"] = 0;
-    translation ["LOAD"] = 1;
-    translation ["ADD"] = 2;
-    translation ["SUB"] = 3;
-    translation ["MUL"] = 4;
-    translation ["DIV"] = 5;
-    translation ["OUT"] = 6;
-    translation ["JMP"] = 7;
-    translation ["BNZ"] = 8;
-    translation ["HALT"] = 9;
 }
 
 void execute_instructions(int opcode){
@@ -273,13 +261,11 @@ void display_menu(){
     cout << "Enter DIV to divide last calculated value by an operand, OR" << endl;
     cout << "Enter OUT to display last calculated value, OR" << endl;
     cout << "Enter HALT to halt program." << endl;
-    //cout << endl;
-    //cout << "Then enter the operand: " <<endl;
 }
 
 char additional_instructions (){
     char response;
-    cout << "Do you have additional instructions? If so, enter 'Y'. " << endl;
+    cout << "Enter additional instructions? If so, enter 'Y'. " << endl;
     cin >> response;
     return response;
 }
